94bdb35ae15439681b420e5480d8a1e3c3c6abc1
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-07-06 11:58:56 +0200

    setup datadog tracing only on production

--- Gemfile
+++ Gemfile
@@ -48,7 +48,7 @@ gem "bootsnap", require: false
 gem "image_processing", "~> 1.2"
 
 # Observability
-gem "datadog", require: "datadog/auto_instrument"
+gem "datadog"
 
 group :development, :test do
   # # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,9 @@
-Datadog.configure do |c|
-  c.env = "local"
-  c.service = "quotes-rails"
-  c.telemetry.enabled = true
-  c.agent.port = 8136
+if Rails.env.production?
+  require "datadog/auto_instrument"
+  Datadog.configure do |c|
+    c.env = "local"
+    c.service = "quotes-rails"
+    c.telemetry.enabled = true
+    c.agent.port = 8136
+  end
 end
