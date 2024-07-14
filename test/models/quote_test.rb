c86eb78798415cf8427c8144f40f0aa2a8a05d6c
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-07-14 16:18:37 +0200

    additional tests for Quote model

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class QuoteTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
   test "should not save quote without name" do
     quote = Quote.new
     assert_not quote.save
@@ -9,5 +11,22 @@ class QuoteTest < ActiveSupport::TestCase
   test "should save quote with name" do
     quote = Quote.new(name: "Test quote")
     assert quote.save
+  end
+
+  test "should broadcast to quotes" do
+    quote = Quote.new(name: "Test quote")
+
+    assert_turbo_stream_broadcasts("quotes", count: 1) do
+      perform_enqueued_jobs do
+        quote.save
+      end
+    end
+  end
+
+  test "should order quotes by newest" do
+    quote1 = Quote.create(name: "First quote")
+    quote2 = Quote.create(name: "Second quote")
+
+    assert_equal [quote2, quote1], Quote.order_newest.limit(2)
   end
 end
