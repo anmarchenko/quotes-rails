0aedbf5b32f7375441bb8084fa6062bf991384e0
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-07-27 18:16:03 +0200

    line item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_131200) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_144356) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -65,6 +65,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_131200) do
     t.index ["quote_id"], name: "index_line_item_dates_on_quote_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer "line_item_date_id", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.integer "quantity", null: false
+    t.decimal "unit_price", precision: 10, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["line_item_date_id"], name: "index_line_items_on_line_item_date_id"
+  end
+
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -96,6 +107,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_131200) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "line_item_dates", "quotes"
+  add_foreign_key "line_items", "line_item_dates"
   add_foreign_key "quotes", "companies"
   add_foreign_key "sessions", "users"
   add_foreign_key "users", "companies"
