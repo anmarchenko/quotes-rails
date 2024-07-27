fa16915ad0fc8620793ba0f1bedf6007737b6c71
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-07-27 16:12:51 +0200

    edit and delete dates

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,6 @@
 class LineItemDatesController < ApplicationController
   before_action :set_quote
+  before_action :set_line_item_date, only: [:edit, :update, :destroy]
 
   def new
     @line_item_date = @quote.line_item_dates.build
@@ -15,6 +16,23 @@ class LineItemDatesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item_date.destroy
+
+    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+  end
+
   private
 
   def line_item_date_params
@@ -22,6 +40,10 @@ class LineItemDatesController < ApplicationController
   end
 
   def set_quote
-    @quote = Current.company.quotes.preload(:line_item_dates).find(params.fetch(:quote_id))
+    @quote = Current.company.quotes.find(params.fetch(:quote_id))
+  end
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
   end
 end
