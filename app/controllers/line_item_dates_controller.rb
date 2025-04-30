4354de90a6b4c97530d8283dae9755728cd9f9d2
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-04-30 12:33:58 +0200

    minor change in controller

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -8,6 +8,7 @@ class LineItemDatesController < ApplicationController
 
   def create
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
+
     @line_item_date.line_items = []
 
     if @line_item_date.save
