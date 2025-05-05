f1d5d97556318294d256ff098d419cd367bfa65e
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-05-05 17:40:35 +0200

    small change

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -14,6 +14,7 @@ class LineItemDatesController < ApplicationController
     if @line_item_date.save
       respond_to do |format|
         format.html { redirect_to quote_path(@quote), notice: "Date was successfully created." }
+
         format.turbo_stream { flash.now[:notice] = "Date was successfully created." }
       end
     else
